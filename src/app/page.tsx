67e6910815dce3f19a183bcc91aeee95121d01a2
ADG08 <dominguesramosandre@gmail.com>
2024-11-26 17:29:12 +0100

    Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-
 export default function Home() {
-    return (
-        <div className="flex items-center justify-center h-screen bg-gray-900">
-            <img
-                src="https://media1.tenor.com/m/OmPA9A9Qz3QAAAAd/august.gif"
-                alt="aze"
-                className="h-screen w-screen"
-            />
-        </div>
-    );
+  return (
+    <>
+      <h1 className="absolute flex justify-center w-screen text-white text-xl">
+        Click et Pafffff
+      </h1>
+      <div className="flex items-center justify-center h-screen bg-gray-900">
+        <img
+          src="https://media1.tenor.com/m/OmPA9A9Qz3QAAAAd/august.gif"
+          alt="aze"
+          className="h-screen w-screen"
+        />
+      </div>
+    </>
+  );
 }
