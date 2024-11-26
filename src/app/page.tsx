0368ebcd4072b3b392ecf1bd4ea9171f8ae399a4
ADG08 <dominguesramosandre@gmail.com>
2024-11-26 17:38:08 +0100

    espace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1 className="absolute flex justify-center w-screen text-white text-xl">
         Click et Pafffff
       </h1>
+
       <div className="flex items-center justify-center h-screen bg-gray-900">
         <img
           src="https://media1.tenor.com/m/OmPA9A9Qz3QAAAAd/august.gif"
